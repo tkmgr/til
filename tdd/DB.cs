507715d1a5da6ceeb91210dca1d01d520474c211
taskmgrdev <taskmgrdev@gmail.com>
2022-12-26 02:27:32 +0900

    feat: TDDテストコード作成

---
+++ tdd/DB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.UI
+{
+    public  class DB : IDB
+    {
+        public int GetDBValue()
+        {
+            return 200;
+        }
+    }
+}
---
+++ tdd/IDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.UI
+{
+    public interface IDB
+    {
+        int GetDBValue();
+    }
+}
